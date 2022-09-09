dba8eec23a298b41e8da6bb95a29388f55aab3ea
Hesty2003 <106217995+Hesty2003@users.noreply.github.com>
2022-09-09 13:57:07 +0700

    Tugas 9

---
+++ system/resources/views/produk/show.blade.php
@@ -0,0 +1,34 @@
+@extends('template.base')
+
+@section('content')
+
+<div class="container">
+            <div class="row">
+                <div class="col md-12 mt-5">
+                    <a href="{{url('admin/produk')}}"><fa class="btn btn-primary">Kembali</a>
+                    <div class="card mt-3">
+                        <div class="card-header">
+                        Detail Data Produk
+                    </div>
+                    <div class="card-body">
+                        <h3>{{$produk->nama}}</h3>
+                        <hr>
+                        <p>
+                            Rp. {{$produk->harga}} |
+                            Stok : {{$produk->stok}} |
+                            Berat : {{$produk->berat}} gr |
+                            Seller : {{ $produk->seller->nama }} |
+                            Tanggal Produksi : {{$produk->created_at->diffForHumans()}}
+                        </p>
+                        <p>
+                            {!! nl2br($produk->deskripsi) !!}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+@endsection
